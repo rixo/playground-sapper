95e2363ea8d74cc3af41f3eb585066aa775ab94f
rixo <rixo@rixo.fr>
2019-11-29 11:40:33 +0100

    add postcss example

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
 
@@ -13,6 +15,16 @@ const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+
+const preprocess = sveltePreprocess({
+	scss: {
+		includePath: ['src', './node_modules'],
+	},
+	postcss: {
+		plugins: [autoprefixer],
+		inject: false,
+	},
+})
 
 export default {
 	client: {
@@ -26,7 +38,8 @@ export default {
 			svelte({
 				dev,
 				hydratable: true,
-				emitCss: true
+				emitCss: true,
+				preprocess,
 			}),
 			resolve({
 				browser: true,
@@ -69,7 +82,8 @@ export default {
 			}),
 			svelte({
 				generate: 'ssr',
-				dev
+				dev,
+				preprocess,
 			}),
 			resolve({
 				dedupe
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,7 +4,18 @@
 	export let segment;
 </script>
 
-<style>
+<style lang="scss" global>
+	// NOTE This is not where I would have wanted to put my global style, but I
+	// failed to configure rollup-plugin-postcss in a compatible way with Svelte.
+	//
+	// WARNING The `global` attribute above is needed here, otherwise the imported
+	// rules will still be scoped by Svelte. As a consequence, that also means
+	// that _any other rules_ in this component (like the `main` bellow) will also
+	// be global. Might be worth it to extract all this into its own Svelte
+	// component with the sole responsability of managing global styles.
+	//
+	@import '../style/shared.scss';
+
 	main {
 		position: relative;
 		max-width: 56em;
